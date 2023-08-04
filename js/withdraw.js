20c70cb9abac7dfc3cab742d206d3d25183b170d
shohanurrahman9361 <shohanur.rahman.cse.bubt@gmail.com>
2023-08-04 18:17:06 +0600

    add new NaN funtion for string typing

--- js/withdraw.js
+++ js/withdraw.js
@@ -7,25 +7,43 @@ document.getElementById('btn-withdraw').addEventListener('click', function() {
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
     console.log(newWithdrawAmount);
+
+      // step 6
+      withdrawField.value = '';
+
+    if(isNaN(newWithdrawAmount)){
+        alert('please provide a valid number');
+        return;
+    }
   
     // step 3
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
   
-    // step 4
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+  
   
     // step 5
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
   
+
+  
+
+if(newWithdrawAmount>previousBalanceTotal){
+    alert('baap er bank e eto taka nai');
+    return ;
+
+}
+
+  // step 4
+  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+  withdrawTotalElement.innerText = currentWithdrawTotal;
+
+
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
   
-    // step 6
-    withdrawField.value = '';
   });
   
